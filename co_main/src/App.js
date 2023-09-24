5ba3de0f0e4f02c1488615fa88abd05a89075a6e
alexziweiwang <wang.ziwei6@northeastern.edu>
2023-09-24 00:10:21 -0700

    add React app part

---
+++ co_main/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is the web-app entry for C.O.
+        </p>
+
+        <p>That is, the user should sign up / log in first, and then manage their projects.</p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
