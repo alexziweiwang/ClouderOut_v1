75411b7a2b2eac73d595e9241b7890e850b68b33
alexziweiwang <wang.ziwei6@northeastern.edu>
2023-09-25 23:41:54 -0700

    React project dev & adjustment

--- co_main/src/App.js
+++ co_main/src/App.js
@@ -1,19 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 import GameMaker from './components/GameMaker.js'
+import Dashboard from './components/Dashboard.js'
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is the web-app entry for C.O.
-        </p>
-
-        <p>That is, the user should sign up / log in first, and then manage their projects.</p>
-        <GameMaker></GameMaker>
-      </header>
+      <Routes>
+        {/* Define your routes within the <Routes> component */}
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/gameediting" element={<GameMaker />} />
+      </Routes>
+      <p>App component</p>
     </div>
   );
 }
