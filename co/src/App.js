ee4d67b01060db53c0529c48eadd3a8152c74f5e
alexziweiwang <wang.ziwei6@northeastern.edu>
2023-09-27 21:44:04 -0700

    add route for invalid path

--- co/src/App.js
+++ co/src/App.js
@@ -11,6 +11,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Dashboard />} />  {/* TODO change later for default entry page */}
+        <Route path="*" element={<Dashboard />} />  {/* TODO change later for default entry page */}
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projectmanagingpanel" element={<ProjectManagerPanel />} />
         <Route path="/gamemaker" element={<GameMaker />} />
