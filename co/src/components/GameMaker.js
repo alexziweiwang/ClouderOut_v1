cf1b53992c4c102452aefe545d65802efd48941a
alexziweiwang <wang.ziwei6@northeastern.edu>
2023-10-03 21:31:35 -0700

    improve arrangement and tasks for game maker

--- co/src/components/GameMaker.js
+++ co/src/components/GameMaker.js
@@ -27,18 +27,29 @@ export default function GameMaker() {
 
     <p className="plans">
       TODO: Entry: Create New Node
+      <br></br> add node name 
+      <br></br> add parent node
+      <br></br> (fill in and see viewer's change)
+    </p>
+
+    <p className="plans">
+      TODO: Entry: Arrange Node order(including Node-editing)
     </p>
     <p className="plans">
-      TODO: Entry: Arrange Nodes (including Node-editing)
-    </p>
-    <p className="plans">
-      drag-and-drop? for pointers and orders between nodes...
+      drag-and-drop? for pointers and orders between nodes... 
+      <br></br>visualization and operation panel: *viewer and setter*
     </p>
     <p className="plans">
       TODO: logic organizer: let users to decide node connection rules
     </p>
 
-    <button class="button" onClick={goToPieceScreenEditingPanel}>Go to PieceScreenEditingPanel</button>
+    <p className="plans">
+      Tasks TODO: 1. visualization and setting of graph of nodes
+      <br></br> 2. solution of logic organizer for nodes
+    </p>
+
+      {/* this can be selected in the future: as a child class of node */}
+    {/* <button class="button" onClick={goToPieceScreenEditingPanel}>Go to PieceScreenEditingPanel</button> */}
 
   </div>
     );
