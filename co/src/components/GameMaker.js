f527372c5d8d77848a59f504c5ae13476eedd2b9
alexziweiwang <wang.ziwei6@northeastern.edu>
2023-10-05 20:34:28 -0700

    organize game-maker on structure and tasks of features

--- co/src/components/GameMaker.js
+++ co/src/components/GameMaker.js
@@ -6,6 +6,12 @@ import styles from './webpage.css';
 
 
 export default function GameMaker() {
+/* // TODO game-maker task list
+1. add "chapter management"
+2. logic organizer for game-node-relationship
+3. preview and test for node play-flow
+*/
+
 
 /* // TODO game-node visualization task list
 1. switching between "new node" and "edit node"
@@ -15,7 +21,8 @@ export default function GameMaker() {
 5. optimization of paths: arrow looking
 6. draggable nodes? (optional)
 7. optimization on node positions when generated
-
+8. adjust svg size, zoom/scrollbar options
+9. game node brief info display and options (hover and click)
 */
 
 
@@ -80,10 +87,26 @@ function goToPieceScreenEditingPanel() {
     
     return (
   <div>
+
+    
     <div className="returning_buttons">
       <button className="button" onClick={goToDashboard}>Return to Dashboard </button>
       <button className="button" onClick={goToProjectManagingPanel}>Return to Project Managing Panel </button>
     </div>
+
+
+    <p className="plans">
+      *** design of interactable graph *** 
+      <br></br>visualization and operation panel: view, hover and double-click
+      <br></br>hover a node would show the starting wording/desciption of this node
+      <br></br>double-click a node would pop an intro window? for options?
+    </p>
+
+    <p className="plans">
+      TODO: logic organizer: let users to fill node-connection-rules *important*
+      <br></br>carry game data to the decision node
+      <br></br>with the "formula" the user created by custom logic organizer, update "next node" assignment of this "decision node"?
+    </p>
 
     <p className="plans">
       TODO: Entry: Create New Node
@@ -174,30 +197,6 @@ function goToPieceScreenEditingPanel() {
 
 
     </div>
-
-    <p className="plans">
-          CONSIDERING solution of "linked list looks": nodes and arrows
-          <br></br>TODO use SVG
-    </p>
-    
-    <p className="plans">
-      *** design of interactable graph *** 
-      <br></br>visualization and operation panel: view, hover and double-click
-      <br></br>hover a node would show the starting wording/desciption of this node
-      <br></br>double-click a node would pop an intro window? for options?
-    </p>
-
-    <p className="plans">
-      TODO: logic organizer: let users to fill node-connection-rules *important*
-      <br></br>carry game data to the decision node
-      <br></br>with the "formula" the user created by custom logic organizer, update "next node" assignment of this "decision node"?
-    </p>
-
-    <p className="plans">
-      -------------- Tasks TODO -------------- 
-      <br></br> 1. visualization and setting of graph of nodes
-      <br></br> 2. solution of logic organizer for nodes
-    </p>
 
 
     {/* <p className="plans">Temp: </p> */}
