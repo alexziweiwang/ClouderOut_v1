75e4ec3bd1077b0c1a8069cb5833a85bbc4481df
alexziweiwang <wang.ziwei6@northeastern.edu>
2023-10-16 22:55:41 -0700

    add collapsible sections for setting: big pic and char pic tested

--- co/src/components/ConversationNodeEditingPanel.js
+++ co/src/components/ConversationNodeEditingPanel.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import styles from './webpage.css';
-import useCollapse from 'react-collapsed';
 
 
 export default function ConversationNodeEditingPanel() {
+
+ 
     const navigate = useNavigate();
 
     const {state} = useLocation();
@@ -14,6 +16,21 @@ export default function ConversationNodeEditingPanel() {
         nodeName = state.selectedNode;
     }
     console.log("this node is : " + nodeName); //TODO
+
+    const [bgpicAdd, setBgPicAdd] = useState(false);
+    const [charPicAdd, setCharPicAdd] = useState(false);
+    const [speakerNameAdd, setSpeakerNameAdd] = useState(false);
+    const [buttonAdd, setButtonAdd] = useState(false);
+    const [bgMusicAdd, setBgMusicAdd] = useState(false);
+    const [voicelineAdd, setVoicelineAdd] = useState(false);
+
+    function switchBgPicOption() {
+        setBgPicAdd(!bgpicAdd);
+    }
+
+    function switchCharPicOption() {
+        setCharPicAdd(!charPicAdd);
+    }
 
     function goToGameMaker() {
         navigate('/gamemaker', { replace: true });
@@ -24,6 +41,7 @@ export default function ConversationNodeEditingPanel() {
         <div>
             <div className="returning_buttons"><button className="button" onClick={goToGameMaker}> Return To GameMaker! </button></div>
             
+            
             <p className="plans">This is conversation-node editing panel
             <br></br> users can do tutorials, or "conversational-like" displaying (As so far planned)
             </p>
@@ -32,18 +50,25 @@ export default function ConversationNodeEditingPanel() {
                 TODO: Add table-like or other solutions for scrolling/switching between slides/pieces for this node
             </p>
 
+            <button onClick={switchBgPicOption}>Add Background Picture</button>
+            {bgpicAdd && <div>TODO: bg pic setting area</div>}
+            <br></br>
+            <button onClick={switchCharPicOption}>Add Character Picture</button>
+            {charPicAdd && <div>TODO: character pic setting area</div>}
+
             <p className="plans">
-                required input data:
-                <br></br> - background picture [0..1]: source link, position, width, height
-                <br></br> - character picture [0..n]: source link, position, width, height
+                required input data: "Edit on Piece"
+
+                <br></br> - [optional] background picture [0..1]: source link, position, width, height
+                <br></br> - [optional] character picture [0..n]: source link, position, width, height
 
                 <br></br> - text content (on UI) [0..1]: content
-                <br></br> - test speaker name [0..1]: content
+                <br></br> - [optional] test speaker name [0..1]: content
 
-                <br></br> - button/clickable item [0..n]: shape/pic_source, sound effect, pressed_consequence on game data
+                <br></br> - [optional] button/clickable item [0..n]: shape/pic_source, sound effect, pressed_consequence on game data
 
-                <br></br> - background music [0..1]: source link, loop or not, volumne
-                <br></br> - voiceline [0..1]: source link, volumne
+                <br></br> - [optional] background music [0..1]: source link, loop or not, volumne
+                <br></br> - [optional] voiceline [0..1]: source link, volumne
 
             </p>
 
