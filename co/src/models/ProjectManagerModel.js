5fc8854273fe7ddb0f3f49672b2a4a4d134527ef
alexziweiwang <wang.ziwei6@northeastern.edu>
2023-10-19 21:43:12 -0700

    improved cloud db function for fetching the list of user's project

--- co/src/models/ProjectManagerModel.js
+++ co/src/models/ProjectManagerModel.js
@@ -1,13 +1,26 @@
 import db from '../googleCloudConnetions';
-import { doc, getDoc, getDocs, collection } from "firebase/firestore"; 
+import { doc, getDoc, getDocs, collection, query } from "firebase/firestore"; 
 
 export async function fetchProjectList() {
+  const currUser = "user002"; //TODO to-change
+
   console.log("model: fetch project list ...");
   
-  const querySnapshot = await getDocs(collection(db, "user_projects"));
-      querySnapshot.forEach((doc) => {
-        console.log(doc.id, " => ", doc.data());
-      });
+  const docRef = doc(db, "user_projects", currUser);
+  const docSnap = await getDoc(docRef);
+
+  if (!docSnap.exists()) {
+    return;
+  }
+
+  const q = query(collection(docRef, "projects"));
+  const querySnapshot = await getDocs(q);
+  let projectArr = [];
+  querySnapshot.forEach((doc) => {
+    projectArr.push(doc.id);
+  });
+
+  console.log("arr:", projectArr); //TODO testing: list of projects for user002
 }
 
 
