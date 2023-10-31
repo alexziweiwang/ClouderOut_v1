b7659ad14f28d1ebc475208b340fc7181929dfd0
alexziweiwang <wang.ziwei6@northeastern.edu>
2023-10-30 21:58:29 -0700

    add: game manager on new variable value adding

--- co/src/components/GameDataManager.js
+++ co/src/components/GameDataManager.js
@@ -12,6 +12,9 @@ export default function GameDataManager({isDisplay, handleGdmCancel, gameData, r
     }
 
     const [displayNewVarArea, setDisplayNewVarArea] = useState(false);
+    const [newGameDataType, setNewGameDataType] = useState("isNumber");
+    const [isNewGdataTypeBoolean, setIsNewDdataTypeBoolean] = useState(false);
+    const [defaultNewBooleanValue, setDefaultNewBooleanValue] = useState(true);
 
 //TODO game-data manager: to add/remove variable, its type, and default value
 
@@ -25,6 +28,24 @@ export default function GameDataManager({isDisplay, handleGdmCancel, gameData, r
         resetNeedCloudData();
         fetchFromCloud();
         setDisplayNewVarArea(false);
+    }
+
+    function selectOnNewGdataType(event) {
+        setNewGameDataType(event.target.value);
+        console.log("selected:", event.target.value); //TODO testing
+        if (event.target.value == "isBoolean") {
+            setIsNewDdataTypeBoolean(true);
+        } else {
+            setIsNewDdataTypeBoolean(false);
+        }
+    }
+
+    function selectOnDefaultBoolean(event) {
+        if (event.target.value == "isTrue") {
+            setDefaultNewBooleanValue(true);
+        } else {
+            setDefaultNewBooleanValue(false);
+        }
     }
 
     return (
@@ -52,6 +73,24 @@ export default function GameDataManager({isDisplay, handleGdmCancel, gameData, r
                         <br></br>data type (options: text, number, true/false)
                         <br></br>default value
                     </p>
+                    <label>Variable Name: </label>
+                    <input></input>
+                    <br></br>
+                    <label>Data Type: </label>
+                    <select value={newGameDataType} onChange={selectOnNewGdataType}>
+                        <option value="isText" key="text"> Text </option>
+                        <option value="isNumber" key="number"> Number </option>
+                        <option value="isBoolean" key="boolean"> True/False </option>
+                    </select>
+                    <br></br>
+                    <label>Default Value: </label>
+                    {!isNewGdataTypeBoolean && <input></input>}
+                    {isNewGdataTypeBoolean && 
+                    <select value={defaultNewBooleanValue} onChange={selectOnDefaultBoolean}>
+                        <option value="isTrue" key="true"> True </option>
+                        <option value="isFalse" key="false"> False </option>
+                    </select>}
+                    <br></br>
                     <button onClick={addNewVarPair}>Submit</button>
                 </div>
             }
