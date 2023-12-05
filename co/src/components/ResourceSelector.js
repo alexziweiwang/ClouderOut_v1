2a30d23576194ab05ec9b7f4dd2008032e0ee4ef
alexziweiwang <wang.ziwei6@northeastern.edu>
2023-12-05 01:40:13 -0800

    adjust UI & edit design on page

--- co/src/components/ResourceSelector.js
+++ co/src/components/ResourceSelector.js
@@ -91,7 +91,8 @@ export default function ResourceSelector ({handleRsCancel, handleRsSaveChanges, 
                     <div className="varPairArea">
                         Variable pair area
                         <p className="plans">
-                            TODO: load list of added variable-pair for *this* project
+                            TODO: load list:
+                            <br></br>cloud db field array name: Variable pair area
                         </p>
 
                         <ul>
@@ -127,21 +128,13 @@ export default function ResourceSelector ({handleRsCancel, handleRsSaveChanges, 
                         })}
                         </ul>
                     </div>
-                <p className="plans">
-                        <br></br>section2: selecting resource name for *this field* ...
-                        <br></br> uesr-flow: defualt panel shows resource-naming selection, and "add new" is at the corner for newly added resource-link, then the main panel get refreshed with the new resource-linking
-                        <br></br>[note: there is no limitaiton on multiple names pointing to the same resource, because it is flexible for later change and adjustment]
 
-                        <br></br> Idea: resource preview should be separate?
-                        <br></br> visual (pictures) and audio (bgm/sound effect/voiceline) preview in different sections
-                        <br></br> TODO: think about separation trategy
-                        <br></br> such as containing a tag(visual or audio), etc.?
-                </p>
                 </div>
 
 
                 <div className="rsrcListArea">
-
+                
+                <label>New Variable Name:</label>
                 <input value={tempVarName} onChange={handleTempVarChange}></input>
                 <br></br><br></br>
                 <label>Visual Resource</label>
@@ -151,7 +144,7 @@ export default function ResourceSelector ({handleRsCancel, handleRsSaveChanges, 
                             itemClicked(item);
                         }}>
                             {<>
-                            <label>      {item["filename"]}</label>      
+                            <label>{item["filename"]}</label>      
                             {!localVisualList.includes(item["filename"]) &&
                             <button onClick={()=>{
                                 let obj = {var: tempVarName, content: item};                                
@@ -201,38 +194,12 @@ export default function ResourceSelector ({handleRsCancel, handleRsSaveChanges, 
                 
                 </div>
 
-                </div>
-
-                <p className="plans">For loaded list: consider option of prewviewing this resource? (the same with ResourceManagingModalWindow)</p>
-
-                <p className="plans">
-                    Here it shouldn't display resource from cloud storage *directly* -- but just the list from naming pair (see resource manager)
-                        <br></br> idea: 
-                        <br></br>section1: adding resource-link (from resource pool) for project-wise [naming] and [actual resource url]
-                        <br></br> (*** db consideration: this link is in range of the entire project, thus consider to put it at the project-layer in database: example: at game-data's level)
-                        <br></br> (data structure: [k, v]: [resource-name, [filename, url]] -- an array of all of these pairs)
-                        <br></br> when creating these link-pairs, the urls are fetched, and stored as value of the pair
-                        <br></br> TODO: consider the assigning of fetched url to variable by UI operations
-                        <br></br>
-                </p>
-              
+                </div>              
                 <p className="plans">
                     for each project, specify the user-given-name for one resource, for modularization and reusability.
                     <br></br> for example, character1 might have pic ver.1 in some art style, and if the author wants to change to ver.2 style, they can do this once by "changing source" for the user-specified preveiewArea
                     <br></br> that is, each resource on cloud storage should have a variable name in this project, and later this name can be reassigned to other resource
                 </p>
-
-                <p className="plans">
-                        TODO: for resource-selection, specific for each project
-                        <br></br> (list all naming pairs)
-                        <br></br>
-                        <br></br> create a naming pair
-                        <br></br> edit a naming pair
-                        <br></br> delete a naming pair
-                        <br></br>
-                        <br></br> This would be done in resource selector?
-
-                    </p>
 
             </div>
 
