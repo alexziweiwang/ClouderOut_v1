59d3c74c416e33791bb0dbcab811677f0b58591d
alexziweiwang <wang.ziwei6@northeastern.edu>
2024-01-15 22:37:17 -0800

    add detailed feature list design

--- co/src/components/GameUISetter.js
+++ co/src/components/GameUISetter.js
@@ -7,6 +7,23 @@ export default function GameUISetter({dataObj}) {
     return(<div className="gameUISettingArea">
         <p className="plans">
             Provide setting options for game menu, game UI, side menut, etc.
+            <br></br>
+            Features:
+            <br></br>* Opening menu
+            <br></br>* in-game side bar
+            <br></br>* in-game menu (pause page)
+            <br></br>* text-content frames
+
+            <br></br>TODO: provide 1 default look, and provide custom options
+            <br></br>customizable: color adjustment
+            <br></br>customizable: background-picture based
+            <br></br>customizable: size
+            <br></br>customizable: position
+            <br></br>customizable: button position
+            <br></br>customizable: button look
+            <br></br><br></br>
+
+            TODO: consider menu setting-list
         </p>
 
 
