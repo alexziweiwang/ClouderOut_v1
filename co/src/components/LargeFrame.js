4331fea6cb4c493df05d82b2621f4b477ecb5dd0
alexziweiwang <wang.ziwei6@northeastern.edu>
2024-02-09 17:40:35 -0800

    add impl-plan for large-frame of sidebar-related pages

--- co/src/components/LargeFrame.js
+++ co/src/components/LargeFrame.js
@@ -7,6 +7,19 @@ export default function LargeFrame({}) {
     //TODO combine sidebar and content pages accordingly
 
     //TODO: user logic: reflect clicked button on sidebar, change on the right-side content section
-        //TODO: optional rendering of left-sidebar and right-content-section
+ 
+    //TODO: optional rendering of left-sidebar and right-content-section
 
+
+    // combinations:
+    //TODO 1. logged in dashboard page(/dashboard) + sidebar
+
+    //TODO 2. account page + sidebar
+
+    //TODO 3. profile page + sidebar
+
+    //TODO 4. project-managing + sidebar2
+
+    //TODO 5. new project + sidebar2
+    
 }
