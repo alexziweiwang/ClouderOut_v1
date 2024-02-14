8aeba7cdb810010a0bed4e8cebd1ee55cd54e3ef
alexziweiwang <wang.ziwei6@northeastern.edu>
2024-02-13 22:14:21 -0800

    add impl-plan and UI entry for the separated vis-preview area

--- co/src/components/PicturePreview.js
+++ co/src/components/PicturePreview.js
@@ -1,4 +1,8 @@
-export default function PicturePreview ({urlList, selectedUrl}) {
+export default function PicturePreview ({urlList, selectedUrl, varPairData, updateVarPairData}) {
+    //TODO use varPairData to display add or edit of variable-pair for this resource in this project
+    //TODO call updateVarPairData when var-pair editted by user
+
+
     return (
         <div>
             {urlList.map((item, index) => {
@@ -13,6 +17,17 @@ export default function PicturePreview ({urlList, selectedUrl}) {
                 </div>);
             }
             )}
+
+            <br></br>
+            <div className="resourceVarPairWindow">Variable Pair management - Visual
+                <br></br>TODO: from varPairData, check if this url is in var-pair record
+                {<> <br></br>
+                    <label>Variable Name: </label>
+                    <input></input> 
+                    {<button>Add</button>}
+                    {<button>Edit</button>}
+                </>}
+            </div>   
         </div>
     );
 }
