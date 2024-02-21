3e0d5d6dda0d4691c644f0298fe275f6c2781327
alexziweiwang <wang.ziwei6@northeastern.edu>
2024-02-21 04:08:27 -0800

    add UI entries and improve layout for Game-UI-settings

--- co/src/components/PieceManager.js
+++ co/src/components/PieceManager.js
@@ -194,12 +194,16 @@ export default function PieceManager({allPieceData, assignPieceNum, assignPrevie
             <br></br><label>Transparency: </label><input></input>
 
             <br></br><input type="radio"></input><label>Rectangle: </label>
+                <div className="indentOne">
                 <label>Button Shade </label><input></input>
+                </div>
             <br></br><input type="radio"></input><label>Base Picture </label>
             <select></select><button>Resource Adding</button>
-            <br></br>Min-Width: <input></input>
-            <br></br>Max-Width: <input></input>
-            <br></br>Height: <input></input>
+            <div className="indentOne">
+                <br></br>Min-Width: <input></input>
+                <br></br>Max-Width: <input></input>
+                <br></br>Height: <input></input>
+            </div>
 
             <br></br><br></br><br></br>
             2. Text Frame
@@ -209,25 +213,43 @@ export default function PieceManager({allPieceData, assignPieceNum, assignPrevie
             <br></br><input type="radio"></input><label>Rectangle: </label>
                 <label>Button Shade </label><input></input>
             <br></br><input type="radio"></input><label>Base Picture </label>
-            <select></select><button>Resource Adding</button>
-            <br></br>Width: <input></input>
-            <br></br>Height: <input></input>
-            <br></br>Position X: <input></input>
-            <br></br>Position Y: <input></input>
-
+            <div className="indentOne">
+                <select></select><button>Resource Adding</button>
+                <br></br>Width: <input></input>
+                <br></br>Height: <input></input>
+                <br></br>Position X: <input></input>
+                <br></br>Position Y: <input></input>
+            </div>
             <br></br><br></br><br></br>
 
             3. In-game Side-bar
             <br></br>Back Button: TODO
             <br></br><labrl>Menu Option: </labrl>   
             <br></br><input type="radio"></input><label>Single Menu Option</label>
-            <br></br>Position X: <input></input>
-            <br></br>Position Y: <input></input>
-            <br></br>TODO: jump to pause page
+            <div className="indentOne">
+                <br></br>Position X: <input></input>
+                <br></br>Position Y: <input></input>
+                <br></br>TODO: jump to pause page
+            </div>
             <br></br><input type="radio"></input><label>Menu Option List</label>
-            <br></br>TODO: select needed options (checkbox items)
-            <br></br><select></select> TODO: vertical or horizontal
-            <br></br>TODO: list position
+            <div className="indentOne">
+                <input type="checkbox"></input><labe>Save</labe>
+                <br></br>
+                <input type="checkbox"></input><labe>Load</labe>
+                <br></br>
+                <input type="checkbox"></input><labe>Settings</labe>
+                <br></br>
+                <input type="checkbox"></input><labe>Return to Title-Page</labe>
+                <br></br>
+                <input type="checkbox"></input><labe>In-Game Data</labe>
+                <br></br>
+                <input type="checkbox"></input><labe>Store</labe>
+                <br></br>
+                <select>
+                    <option key="v" value="vertical">Vertical</option>    
+                    <option key="h" value="horizontal">Horizontal</option>    
+                </select>
+            </div>
             <br></br>Position X: <input></input>
             <br></br>Position Y: <input></input>
 
