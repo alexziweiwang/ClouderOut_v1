6b4c4e3c3ed5e22688eb3cc4ad4286ffaddf5d70
alexziweiwang <wang.ziwei6@northeastern.edu>
2024-02-27 00:12:55 -0800

    implement radio-option in individual-button in Game-UI-setter

--- co/src/components/GameUISetter.js
+++ co/src/components/GameUISetter.js
@@ -40,10 +40,10 @@ export default function GameUISetter({}) {
                                 </select>
     <br></br><label>Transparency: </label><input type="range" value={idvButtonTransparency} min="0" max="100" step="1" defaultValue="90" onChange={(event)=>{setIdvButtonTransparency(event.target.value);}}></input><label>{idvButtonTransparency}%</label>
 
-    <br></br><input type="radio"></input><label>Rectangle: </label>
+    <br></br><input type="radio" value={idvButtonIsShape} checked={idvButtonIsShape} onChange={(event)=>{setIdvButtonIsShape(true);}}></input><label>Rectangle: </label>
         <input type="color"></input><label>[shade value]</label>
         
-    <br></br><input type="radio"></input><label>Base Picture </label><select></select><button>Resource Adding</button>
+    <br></br><input type="radio" value={idvButtonIsShape} checked={!idvButtonIsShape} onChange={(event)=>{setIdvButtonIsShape(false);}}></input><label>Base Picture </label><select></select><button>Resource Adding</button>
 
 
     <br></br><br></br><br></br>
