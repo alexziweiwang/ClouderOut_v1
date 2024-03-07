d630ab329ec16bae43a1884ae8cd967d1b0cbc04
alexziweiwang <wang.ziwei6@northeastern.edu>
2024-03-06 18:14:57 -0800

    Game-UI-Setter: responsive areas for menu-shape/pic

--- co/src/components/GameUISetter.js
+++ co/src/components/GameUISetter.js
@@ -144,10 +144,11 @@ export default function GameUISetter({}) {
             <option value="1" key="1">1</option>
             <option value="5" key="5">5</option>
         </select>
-    <br></br><input type="radio" value={igsidebarMenuIsShape} checked={igsidebarMenuIsShape} onChange={()=>{setIgsidebarMenuIsShape(true);}}></input><label>Rectangle: </label>
-    <input type="color" value={igsidebarMenuShade} onChange={(event)=>{setIgsidebarMenuShade(event.target.value);}}></input><label>{igsidebarMenuShade}</label>
+    <br></br><input type="radio" value={igsidebarMenuIsShape} checked={igsidebarMenuIsShape} onChange={()=>{setIgsidebarMenuIsShape(true);}}></input><label onChange={()=>{setIgsidebarMenuIsShape(true);}}>Rectangle: </label>
+    {igsidebarMenuIsShape === true && <><input type="color" value={igsidebarMenuShade} onChange={(event)=>{setIgsidebarMenuShade(event.target.value);}}></input><label>{igsidebarMenuShade}</label></>}
     <br></br>
-    <input type="radio" value={igsidebarMenuIsShape} checked={!igsidebarMenuIsShape} onChange={()=>{setIgsidebarMenuIsShape(false);}}></input><label>Base Picture</label><select></select><button>Resource Adding</button>
+    <input type="radio" value={igsidebarMenuIsShape} checked={!igsidebarMenuIsShape} onChange={()=>{setIgsidebarMenuIsShape(false);}}></input><label onChange={()=>{setIgsidebarMenuIsShape(false);}}>Base Picture: </label>
+    {igsidebarMenuIsShape === false && <><select></select><button>Resource Adding</button></>}
     
         <br></br><br></br>
     <br></br><input type="radio" value={igsidebarMenuIsSingleBtn} checked={igsidebarMenuIsSingleBtn} onChange={()=>{setIgsidebarMenuIsSingleBtn(true);}}></input><label>Single Menu Option</label>
