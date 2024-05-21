9ce48559baa0801552915b66d2a36b80b0730e62
alexziweiwang <wang.ziwei6@northeastern.edu>
2024-05-21 13:12:57 -0700

    add screens to preview area with separate data entries

--- co/src/components/NavigationPreview.js
+++ co/src/components/NavigationPreview.js
@@ -2,6 +2,9 @@ import { useState, useEffect } from 'react';
 
 
 export default function NavigationPreview({initialNavObj, fetchNavObj}) {
+    const screenWidth = 800;
+    const screenHeight = 450;
+
 
     const [navObj, setNavObj] = useState(initialNavObj);
 
@@ -22,16 +25,38 @@ export default function NavigationPreview({initialNavObj, fetchNavObj}) {
         <br></br>
         {navObj["isWithSL"].toString()} <br></br>
 
-        {navObj["mainPage-story"].toString()}<br></br>
-        {navObj["mainPage-shop"].toString()}<br></br>
-        {navObj["mainPage-setting"].toString()}<br></br>
-        {navObj["mainPage-playerProfile"].toString()} <br></br>
-        {navObj["mainPage-entriesHorizontal"].toString()}<br></br>
-        {navObj["mainPage-entriesCustom"].toString()}<br></br>
-        {navObj["settingPage-playSpeed"].toString()}<br></br>
-        {navObj["settingPage-bgmVol"].toString()}<br></br>
-        {navObj["settingPage-seVol"].toString()}<br></br>
-        {navObj["settingPage-entriesHorizontal"].toString()}<br></br>
-        {navObj["settingPage-entriesCustom"].toString()}
+
+        <br></br>
+        <div style={{"width": `${screenWidth}px`, "height": `${screenHeight}px`,"backgroundColor": "rgb(194, 223, 194)", "marginLeft": `20px`}}
+            >
+            main page
+            <br></br>
+            {navObj["mainPage-story"].toString()}<br></br>
+            {navObj["mainPage-shop"].toString()}<br></br>
+            {navObj["mainPage-setting"].toString()}<br></br>
+            {navObj["mainPage-playerProfile"].toString()} <br></br>
+            {navObj["mainPage-entriesHorizontal"].toString()}<br></br>
+            {navObj["mainPage-entriesCustom"].toString()}<br></br>
+        </div>
+        <br></br>
+        <div style={{"width": `${screenWidth}px`, "height": `${screenHeight}px`,"backgroundColor": "rgb(194, 223, 194)", "marginLeft": `20px`}}
+        >
+            story page
+            <br></br>        
+            {navObj["storyPage-chapterListHorizontal"].toString()}<br></br>
+        </div>
+        <br></br>
+        <div style={{"width": `${screenWidth}px`, "height": `${screenHeight}px`,"backgroundColor": "rgb(194, 223, 194)", "marginLeft": `20px`}}
+        >
+            setting page
+            <br></br>   
+            {navObj["settingPage-playSpeed"].toString()}<br></br>
+            {navObj["settingPage-bgmVol"].toString()}<br></br>
+            {navObj["settingPage-seVol"].toString()}<br></br>
+            {navObj["settingPage-entriesHorizontal"].toString()}<br></br>
+            {navObj["settingPage-entriesCustom"].toString()} 
+        </div>
+
+
     </div>);
 }
