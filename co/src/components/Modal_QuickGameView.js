eab503701dae5d6af92660b7b24ca87623eb2cd9
alexziweiwang <wang.ziwei6@northeastern.edu>
2024-07-12 22:57:11 -0700

    implement q-view for bgp,bgm,charPics

--- co/src/components/Modal_QuickGameView.js
+++ co/src/components/Modal_QuickGameView.js
@@ -18,20 +18,24 @@ export default function Modal_QuickGameView ({handleQViewCancel, isDisplay, scre
 
     const [audioMap, setAudioMap] = useState({});
     const [visualMap, setVisualMap] = useState({}); 
-  
     const [audioMapSize, setAudioMapSize] = useState(0);
     const [visualMapSize, setVisualMapSize] = useState(0);
+    
+    const [bgmSource, setBgmSource] = useState("");
+    const [bgpSource, setBgpSource] = useState("");
 
     const [charaPicArr2, setCharaPicArr2] = useState(allPieceContent[0]["chp_arr"]);
 
-    // const [firstTimeEnter, setFirstTimeEnter] = useState(true);   //TODO temp
+    const [firstTimeEnter, setFirstTimeEnter] = useState(true);   //TODO temp
     useEffect(() => {
  
-//       if (firstTimeEnter === true) {
-//         setFirstTimeEnter(false);
+      if (firstTimeEnter === true) {
+        setFirstTimeEnter(false);
 
-// console.log("game-q-view entry, resource-list fetched."); //TODO temp
-//       }
+console.log("game-q-view first entry"); //TODO temp
+console.log("all-piece-content = ", allPieceContent); //TODO testing
+
+      }
 
 
 
@@ -46,9 +50,9 @@ export default function Modal_QuickGameView ({handleQViewCancel, isDisplay, scre
         }
 
       
-      updateCharPicArr(allPieceContent, currPieceNum);
-      updateBgmSource(allPieceContent, currPieceNum);
-      updateBgpSource(allPieceContent, currPieceNum);
+      updateCharPicArr();
+      updateBgmSource();
+      updateBgpSource();
       
 
         if (audioMapSize < audioList.length || visualMapSize < visualList.length) {
@@ -85,8 +89,40 @@ export default function Modal_QuickGameView ({handleQViewCancel, isDisplay, scre
           } 
     }
 
+    function updateBgmSource() {
+        if (currPieceNum < 0) {
+            return;
+          }
+          if (allPieceContent[currPieceNum]["bgm_action"] === "startNewBgm") {
+            if (allPieceContent[currPieceNum]["bgm_source_varname"] !== "") {
+              setBgmSource(audioMap[allPieceContent[currPieceNum]["bgm_source_varname"]]);
+            }
+          } else if (allPieceContent[currPieceNum]["bgm_action"] === "stopBgm") {
+            setBgmSource("");
+          } 
+          //TODO "naturalStopBgm" stop looping...
+
+    }
+
+    function updateBgpSource() {
+        if (currPieceNum < 0) {
+          return;
+        }
+        if (allPieceContent[currPieceNum]["bgp_action"] === "switchToNewBgp") {
+          if (allPieceContent[currPieceNum]["bgp_source_varname"] !== "") {
+            setBgpSource(visualMap[allPieceContent[currPieceNum]["bgp_source_varname"]]);
+          } else {
+            setBgpSource("");
+          }
+        
+        } 
+      }    
+
+
     function triggerNextPiece() {
-        setCurrPieceNum(currPieceNum+1);
+        if (allPieceContent[currPieceNum+1] !== undefined) {
+            setCurrPieceNum(currPieceNum+1);
+        }
     }
 
 
