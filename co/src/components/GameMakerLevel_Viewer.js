cd3154034df4e9ef8911b1e4ffec837626bb2815
alexziweiwang <wang.ziwei6@northeastern.edu>
2024-08-08 18:00:17 -0700

    add and plan for functionalities on game-maker-level viewer: large frame

--- co/src/components/GameMakerLevel_Viewer.js
+++ co/src/components/GameMakerLevel_Viewer.js
@@ -23,9 +23,26 @@ export default function GameMakerLevel_Viewer({isDisplay, makeNotDisplay, naviga
         modalStyleName = "displayNone modalBackboard";
     }
 
+    const [navStatus, setNavStatus] = useState("Main Page");
+
+    const [gameDataTracker, setGameDataTracker] = useState(); //TODO add default from user's setting; pass-in to callee components (game nodes)
+
+    const [currChapterKey, setCurrChapterKey] = useState("");
+    
+    const [currNodeType, setCurrNodeType] = useState(""); //TODO according to node-type, display the correct node's viewer?
 
 
+    function getNavStatus(word) {
+        setNavStatus(word);
+    }
 
+    function updateGameData(data) { // model's functionality
+        setGameDataTracker(data);
+    }
+
+    function passInGameData() {
+        return gameDataTracker;
+    }
 
 return(<>
 
@@ -35,11 +52,16 @@ return(<>
         <button onClick={()=>{makeNotDisplay();}}>Close</button>
     ~GameMakerLevel_Viewer~
 
-        <div style={{"position": "absolute"}}>placeholder: game node (content+UI)</div>
+        {navStatus === "During Game" && <div style={{"position": "absolute"}}>
+            placeholder: game node (content+UI)
+            
+            <br></br>TODO: according to currNodeType, display the component
+        </div>}
 
         <div className="plans" style={{"position": "absolute"}}>
             placeholder: navigation/UI system
             <br></br>with "navigationObj"
+            <br></br>TODO: send in getNavStatus() for current nav-page
 
         </div>
 
