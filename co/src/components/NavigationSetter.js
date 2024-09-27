aa623b12d4c77aaaa7508768c58b6eb8aa0e8bb6
alexziweiwang <wang.ziwei6@northeastern.edu>
2024-09-26 22:36:17 -0700

    implement pp-icon height

--- co/src/components/NavigationSetter.js
+++ co/src/components/NavigationSetter.js
@@ -4050,7 +4050,25 @@ export default function NavigationSetter({initialNavObj,
                   }}
                 ></input>
                 <br></br>
-                <label>Height: </label><input type="range"></input><input></input>
+                <label>Height: </label>
+                <input type="range"
+                  min="0" max={screenHeight} step="1"
+                  onChange={(event)=>{
+                    let tempNav = currentProjectNav;
+                    tempNav["playerProfilePage-playerProfileIconPicItem"]["height"] = event.target.value;
+                    setCurrentProjectNav({...currentProjectNav, "playerProfilePage-playerProfileIconPicItem": tempNav["playerProfilePage-playerProfileIconPicItem"]});         
+                    updateNavObj(tempNav);                       
+                  }}
+                ></input>
+                <input
+                  min="0" max={screenHeight} step="1"
+                  onChange={(event)=>{
+                    let tempNav = currentProjectNav;
+                    tempNav["playerProfilePage-playerProfileIconPicItem"]["height"] = event.target.value;
+                    setCurrentProjectNav({...currentProjectNav, "playerProfilePage-playerProfileIconPicItem": tempNav["playerProfilePage-playerProfileIconPicItem"]});         
+                    updateNavObj(tempNav);                       
+                  }}
+                ></input>
             </div>}
                       
 
