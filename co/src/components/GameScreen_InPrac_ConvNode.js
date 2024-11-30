445b0c46765a2dca6506f14e5e3bc6d774dca2d4
alexziweiwang <wang.ziwei6@northeastern.edu>
2024-11-29 19:15:32 -0800

    implement new compo, setup, part1

--- co/src/components/GameScreen_InPrac_ConvNode.js
+++ co/src/components/GameScreen_InPrac_ConvNode.js
@@ -19,12 +19,111 @@ export default function ({
     visualList,
     audioList,
 
-    
+
     buttonConsequenceByStatementEntireArray,
 }) {
 
     const initialPieceNum = 0;
     const isDisplay = true;
+
+    const [currPieceNum, setCurrPieceNum] = useState(initialPieceNum);
+    const [directNextPieceBool, setDirectNextPieceBool] = useState(true);
+    const [textStillTyping, setTextStillTyping] = useState(true);
+    const [immediateFinishSignal, setImmediateFinishSignal] = useState(false);
+    const [autoMode, setAutoMode] = useState(false);
+
+    const [audioMap, setAudioMap] = useState({});
+    const [visualMap, setVisualMap] = useState({}); 
+    const [audioMapSize, setAudioMapSize] = useState(0);
+    const [visualMapSize, setVisualMapSize] = useState(0);
+    
+    const [bgmSource, setBgmSource] = useState("");
+    const [bgpSource, setBgpSource] = useState("");
+
+    const [charaPicArr2, setCharaPicArr2] = useState(allPieceContent[0]["chp_arr"]);
+
+    const [showConvLog, setShowConvLog] = useState(false);
+
+
+    const [firstTimeEnter, setFirstTimeEnter] = useState(true);   //TODO temp
+    useEffect(() => {
+ 
+        if (firstTimeEnter === true) {
+
+            setFirstTimeEnter(false);
+        }
+
+        updateCharPicArr();
+        updateBgmSource();
+        updateBgpSource();
+        
+
+        if (audioMapSize < audioList.length || visualMapSize < visualList.length) {
+            let i = 0;
+            let tempAudioMap = {};
+            setAudioMapSize(audioList.length);
+            for (;i < audioList.length; i++) {
+                let item = audioList[i];
+                tempAudioMap[item["var"]] = item["url"];
+            }
+            setAudioMap(tempAudioMap);
+
+            i = 0;
+            let tempVisualMap = {};
+            setVisualMapSize(visualList.length);
+            for (;i < visualList.length; i++) {
+                let item = visualList[i];
+                tempVisualMap[item["var"]] = item["url"];
+            }
+            setVisualMap(tempVisualMap);
+        }
+
+
+        if (allPieceContent[currPieceNum].displayTextFrame === false) {
+            setTextStillTyping(false);
+        }
+
+    }); //-- useEffect --
+
+
+    function updateCharPicArr() {
+        if (currPieceNum < 0) {
+            return;
+          }
+          
+          if (allPieceContent[currPieceNum]["chp_action"] === "changeCharPicArr") {     
+            setCharaPicArr2(allPieceContent[currPieceNum]["chp_arr"]);  
+          } 
+    }
+
+    function updateBgmSource() {
+        if (currPieceNum < 0) {
+            return;
+          }
+          if (allPieceContent[currPieceNum]["bgm_action"] === "startNewBgm") {
+            if (allPieceContent[currPieceNum]["bgm_source_varname"] !== "") {
+              setBgmSource(audioMap[allPieceContent[currPieceNum]["bgm_source_varname"]]);
+            }
+          } else if (allPieceContent[currPieceNum]["bgm_action"] === "stopBgm") {
+            setBgmSource("");
+          } 
+          //TODO "naturalStopBgm" stop looping...
+
+    }
+
+    function updateBgpSource() {
+        if (currPieceNum < 0) {
+          return;
+        }
+        if (allPieceContent[currPieceNum]["bgp_action"] === "switchToNewBgp") {
+          if (allPieceContent[currPieceNum]["bgp_source_varname"] !== "") {
+            setBgpSource(visualMap[allPieceContent[currPieceNum]["bgp_source_varname"]]);
+          } else {
+            setBgpSource("");
+          }
+        
+        } 
+    }    
 
 
 return (<div>
