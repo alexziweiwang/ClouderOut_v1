080f84065afeb3f4debf1e31456113178fa34072
alexziweiwang <wang.ziwei6@northeastern.edu>
2025-05-29 22:33:06 -0700

    adjust UI and func for account-page, part 1

--- co/src/components/AccountPage.js
+++ co/src/components/AccountPage.js
@@ -23,6 +23,27 @@ export default function AccountPage({}) {
     const [authEmailName, setAuthEmailName] = useState("_");
 
 
+    const [nicknameInput, setNicknameInput] = useState("");
+    const [emailLinkedInput, setEmailLinkedInput] = useState("");
+    const [phonenumberInput, setPhonenumberInput] = useState("");
+
+    const [nicknameChanging, setNicknameChanging] = useState(false);
+    const [emailLinkedChanging, setEmailLinkedChanging] = useState(false);
+    const [phonenumberChanging, setPhonenumberChanging] = useState(false);
+
+    function handleNicknameChange(event) {
+
+    }
+
+    function handleEmailLinkChange(event) {
+
+    }
+
+    function handlePhonenumberChange(event) {
+
+    }
+
+
     const [firstTimeEnter, setFirstTimeEnter] = useState(true);   //TODO temp
     useEffect(() => {
 
@@ -69,33 +90,61 @@ export default function AccountPage({}) {
 
     <div className="dashboard_content">
         <div style={{"textAlign": "left", "padding": "10px"}}>
-          <table className="noBorder">
+          <table className="noBorder" style={{"width": "1200px"}}>
        
             <tbody>
-              <tr>
-                <td className="noBorder">Username: </td>
-                <td className="noBorder">(username value)</td>
+
+              <tr style={{"backgroundColor": "green", "width": "200px"}}>
+                  <td className="noBorder" style={{"width": "100px"}}>Username: </td>
+                  <td className="noBorder" style={{"width": "500px"}}>(username value)</td>
               </tr>
 
               <tr>
-                <td className="noBorder">Nickname: </td>
-                <td className="noBorder">(nickname value) <button>Edit</button></td>
+                  <td className="noBorder">Nickname: </td>
+                  <td className="noBorder">(nickname value) 
+                  
+                  
+                      {nicknameChanging === false && <button onClick={()=>{setNicknameChanging(true);}}>Edit</button>}
+                      {nicknameChanging === true && <>
+                        <input></input>
+                        <button onClick={()=>{setNicknameChanging(false);}}>Cancel</button>
+                        <button>Save</button>
+                      </>}
+                      
+                  </td>
               </tr>
 
               <tr>
-                <td className="noBorder">Email linked: </td>
-                <td className="noBorder">(email value) <button>Edit</button></td>
+                  <td className="noBorder">Email linked: </td>
+                  <td className="noBorder">(email value) 
+                      {emailLinkedChanging === false && <button onClick={()=>{setEmailLinkedChanging(true);}}>Edit</button>}
+                      {emailLinkedChanging === true && <>
+                        <input></input>
+                        <button onClick={()=>{setEmailLinkedChanging(false);}}>Cancel</button>
+                        <button>Save</button>
+                      
+                      </>}
+                  </td>
               </tr>
 
               <tr>
-                <td className="noBorder">Phone number linked: </td>
-                <td className="noBorder">(phone number value) <button>Edit</button></td>
+                  <td className="noBorder">Phone number linked: </td>
+                  <td className="noBorder">(phone number value) 
+                  
+                      {phonenumberChanging === false && <button onClick={()=>{setPhonenumberChanging(true)}}>Edit</button>}
+                      {phonenumberChanging === true && <>
+                        <input></input>
+                        <button onClick={()=>{setPhonenumberChanging(false);}}>Cancel</button>
+                        <button>Save</button>
+                      
+                      
+                      
+                      </>}
+                        
+                  </td>
               </tr>
 
-              <tr>
-                <td className="noBorder">Clouder-Credits:</td>
-                <td className="noBorder">(credit value <button>Top Up</button></td>
-              </tr>
+
             </tbody>
             
 
